2208c05c665423cf95bc9c8bb182a8c3fe51a785
Stanley Stout <stouts@mac.com>
2016-07-29 10:03:31 -0500

    Initial Commit

---
+++ Last Encounter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Last Encounter
+//
+//  Created by Stanley Stout on 7/29/16.
+//  Copyright © 2016 Chris Stout. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
