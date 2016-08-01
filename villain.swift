acf1b5931a75a4efc2f56fbf77ed992b67408805
Alex Israelov <alex1115alex@gmail.com>
2016-08-01 10:03:16 -0500

    remade villain

---
+++ villain.swift
@@ -0,0 +1,19 @@
+//
+//  villain.swift
+//  Last Encounter
+//
+//  Created by Alex Israelov on 8/1/16.
+//  Copyright © 2016 Chris Stout. All rights reserved.
+//
+
+import UIKit
+
+class villain: NSObject {
+
+    var health = 10;
+    var attack = 10;
+    var mana = 10
+    var magic  = 10;
+    var defense = 10;
+    
+}
