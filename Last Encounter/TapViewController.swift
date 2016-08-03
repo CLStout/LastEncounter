e07341a97b53b833d23f90481ac8e0107220d31d
Alex Israelov <alex1115alex@gmail.com>
2016-08-03 11:22:37 -0500

    did more ui stuff

---
+++ Last Encounter/TapViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TapViewController.swift
+//  Last Encounter
+//
+//  Created by Alex Israelov on 8/3/16.
+//  Copyright © 2016 Chris Stout. All rights reserved.
+//
+
+import UIKit
+
+class TapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
